68a77edd00c856157aa9d12968f11e4775c6f8da
2022Dong <faithhopelove777111@gmail.com>
2024-10-26 21:00:32 +0800

    Client Component & Navigation Hooks

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,26 @@
+'use client'; // Client side.
+import { useRouter, useParams, useSearchParams, usePathname} from 'next/navigation';
+
 const PropertyPage = () => {
-    return ( <div>Property Page</div> );
+    const router = useRouter();
+    // return (
+    //     <div>
+    //         <button onClick={() => router.replace('/')}>
+    //             Go Home
+    //         </button>
+    //     </div>
+    //  );
+    
+    const params = useParams();
+    // return (
+    //     <div>Property Page {params.id}</div>  //[slug] --- {params.slug}
+    // );
+
+    const searchParams = useSearchParams();
+    // return <div>Property Page {searchParams.get('name') }</div> // http://localhost:3000/properties/1?name=Aya
+
+    const pathname = usePathname();
+    // return <div>Property Page {pathname}</div>
 }
  
 export default PropertyPage;
