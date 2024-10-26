7c3642360f2f3b1a37f33c225aaa71f01653edc6
2022Dong <faithhopelove777111@gmail.com>
2024-10-26 21:07:13 +0800

    Server...

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,26 +1,6 @@
-'use client'; // Client side.
-import { useRouter, useParams, useSearchParams, usePathname} from 'next/navigation';
-
-const PropertyPage = () => {
-    const router = useRouter();
-    // return (
-    //     <div>
-    //         <button onClick={() => router.replace('/')}>
-    //             Go Home
-    //         </button>
-    //     </div>
-    //  );
-    
-    const params = useParams();
-    // return (
-    //     <div>Property Page {params.id}</div>  //[slug] --- {params.slug}
-    // );
-
-    const searchParams = useSearchParams();
-    // return <div>Property Page {searchParams.get('name') }</div> // http://localhost:3000/properties/1?name=Aya
-
-    const pathname = usePathname();
-    // return <div>Property Page {pathname}</div>
+const PropertyPage = ({ params, searchParams}) => {
+    // return <div>Property Page {params.id}</div>
+    // return <div>Property Page {searchParams.name}</div>
 }
  
 export default PropertyPage;
