00352d8eb5ae522059d5ce0166fbf36697e58656
2022Dong <faithhopelove777111@gmail.com>
2024-10-30 09:36:13 +0800

    Fetch Data Using Server Component

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,8 +1,15 @@
-import properties from '@/properties.json';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
 import PropertyCard from '@/components/PropertyCard';
 import Link from 'next/link';
 
-const HomeProperties = () => {
+const HomeProperties = async () => {
+    await connectDB();
+    const recentProperties = await Property.find({})
+        .sort({ createAt: -1 })
+        .limit(3)
+        .lean();
+
     return ( 
         <>        
         <section className='px-4 py-6'>
@@ -11,11 +18,11 @@ const HomeProperties = () => {
                     Recent Properties
                 </h2>
 
-            {properties.length === 0 ? (
+            {recentProperties.length === 0 ? (
             <p>No properties found</p>
             ) : (
              <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                {properties.slice(0, 3).map((property, index) => (
+                {recentProperties.map((property, index) => (
                   <PropertyCard key={index} property={property} />
                 ))}
              </div>
