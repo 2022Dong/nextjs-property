4eaa352f2f50a64d1cc7806b34da36c2f1c102a7
2022Dong <faithhopelove777111@gmail.com>
2024-10-30 08:33:21 +0800

    connect mongoDB

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
+import connectDB from "@/config/database";
 
 const HomePage = () => {
+    //console.log(process.env.MONGODB_URI);  // display result in terminal
+    connectDB();
     return ( 
         <>
             <Hero />
