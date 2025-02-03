a7406c66ac263a0e305e3cd88c49988cc65a136d
Dong <20127545@tafe.wa.edu.au>
2025-02-03 22:39:55 +1100

    Display & Mark As Read & Delete & Global Context For Message Count & Get Unread Message Count

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
 import { ToastContainer  } from "react-toastify";
+import { GlobalProvider } from "@/context/GlobalContext";
 import 'react-toastify/dist/ReactToastify.css';
 import "@/assets/styles/globals.css";
 
@@ -14,6 +15,7 @@ export const metadata = {
 const MainLayout = ({ children }) => {
   return (
     <AuthProvider>
+      <GlobalProvider>
       <html>
         <body>
           <main>
@@ -24,6 +26,7 @@ const MainLayout = ({ children }) => {
           </main>
         </body>
       </html>
+      </GlobalProvider>
     </AuthProvider>
   );
 };
